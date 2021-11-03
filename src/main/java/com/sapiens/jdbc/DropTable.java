356d9279648a99269adfb54e8b78fd0c6aa4b3da
AkaberSapiens <Akaber.Zalaf@sapiens.com>
2021-11-03 08:15:13 +0200

    drop a table

---
+++ src/main/java/com/sapiens/jdbc/DropTable.java
@@ -0,0 +1,30 @@
+package com.sapiens.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
+
+public class DropTable {
+
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+
+
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/emp", "root", "root");
+        System.out.println("Connection Established Successfully to emp DB");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Chooses Table Name You Want To Drop:");
+        String tableName = scanner.next();
+
+        Statement statement = connection.createStatement();
+        String sql = "DROP TABLE "+ tableName;
+        statement.executeUpdate(sql);
+
+        System.out.println("Table was dropped successfully");
+        connection.close();
+
+    }
+}
